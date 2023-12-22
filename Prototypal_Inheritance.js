950ceb161d3859bed9c4d0e6087c3140bc062b98
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-22 16:42:01 +0530

    task-Why are both hamsters full?

--- Prototypal_Inheritance.js
+++ Prototypal_Inheritance.js
@@ -99,6 +99,32 @@ let animal1= {
   
 console.log(rabbit1.eat());
 
+// task-Why are both hamsters full?
+let hamster = {
+    stomach: [],
+  
+    eat(food) {
+      this.stomach.push(food);
+    }
+  };
+  
+  let speedy = {
+    __proto__: hamster,
+    stomach: []
+  };
+  
+  let lazy = {
+    __proto__: hamster,
+    stomach: []
+  };
+  
+  // Speedy one found the food
+  speedy.eat("apple");
+   console.log( speedy.stomach ); // apple
+  
+  // Lazy one's stomach is empty
+   console.log( lazy.stomach ); // []
+
 
 
 

22f6ebcab2b6c2e8434ea66a3b15ac804ab6aae7
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-22 14:06:24 +0530

    assignment is handled by a setter function(admin.fullName)

--- Prototypal_Inheritance.js
+++ Prototypal_Inheritance.js
@@ -1,18 +1,43 @@
-obj1 = {
-    name : "vinay",
-    pc : "hp"
-}
+// obj1 = {
+//     name : "vinay",
+//     pc : "hp"
+// }
 
-obj2 = {
-    name:"harry",
-    car:"audi",
-    dine() {
-        return("at hotel only")
+// obj2 = {
+//     name:"harry",
+//     car:"audi",
+//     dine() {
+//         return("at hotel only")
+//     }
+// }
+
+// obj1.__proto__ = obj2;
+
+// console.log(obj1.car);
+// console.log(obj2);
+// console.log(obj1.dine());
+
+// assignment is handled by a setter function(admin.fullName)
+let user = {
+    name: "messi",
+    surName: "arsenal",
+  
+    set fullName(value) {
+      [this.name, this.surName] = value.split(" ");
+    },
+    get fullName() {
+      return `${this.name} ${this.surName}`;
     }
-}
+  };
+  
+  let admin = {
+    __proto__: user,
+    isAdmin: true
+  };  
+  
+  console.log(admin.fullName);
+  admin.fullName = "vin beelagi";
+  console.log(admin.fullName);
+  console.log(user.fullName);  
 
-obj1.__proto__ = obj2;
 
-console.log(obj1.car);
-console.log(obj2);
-console.log(obj1.dine());

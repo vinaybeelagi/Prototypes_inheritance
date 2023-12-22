d1ca83bf3c46683e9258eac0c4cb95941fbc93e2
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-22 17:34:55 +0530

    task-Create an object with the same constructor

--- function_Prototype.js
+++ function_Prototype.js
@@ -1,48 +1,59 @@
-//task- Changing "prototype"
-//1.
-function Rabbit() {}
-Rabbit.prototype = {
-  eats: true
-};
+// //task- Changing "prototype"
+// //1.
+// function Rabbit() {}
+// Rabbit.prototype = {
+//   eats: true
+// };
 
-let rabbit = new Rabbit();
+// let rabbit = new Rabbit();
 
-Rabbit.prototype = {};
+// Rabbit.prototype = {};
 
-alert( rabbit.eats ); // true
+// alert( rabbit.eats ); // true
 
-//2.
-function Rabbit() {}
-Rabbit.prototype = {
-  eats: true
-};
+// //2.
+// function Rabbit() {}
+// Rabbit.prototype = {
+//   eats: true
+// };
 
-let rabbit = new Rabbit();
+// let rabbit = new Rabbit();
 
-Rabbit.prototype.eats = false;
+// Rabbit.prototype.eats = false;
 
-alert( rabbit.eats ); //false
+// alert( rabbit.eats ); //false
 
-//3.
-function Rabbit() {}
-Rabbit.prototype = {
-  eats: true
-};
+// //3.
+// function Rabbit() {}
+// Rabbit.prototype = {
+//   eats: true
+// };
 
-let rabbit = new Rabbit();
+// let rabbit = new Rabbit();
 
-delete rabbit.eats;
+// delete rabbit.eats;
 
-alert( rabbit.eats ); // true
+// alert( rabbit.eats ); // true
 
-//3.
-function Rabbit() {}
-Rabbit.prototype = {
-  eats: true
-};
+// //3.
+// function Rabbit() {}
+// Rabbit.prototype = {
+//   eats: true
+// };
 
-let rabbit = new Rabbit();
+// let rabbit = new Rabbit();
 
-delete Rabbit.prototype.eats;
+// delete Rabbit.prototype.eats;
 
-alert( rabbit.eats ); // undefined
+// alert( rabbit.eats ); // undefined
+
+// task-Create an object with the same constructor
+function User(name) {
+    this.name = name;
+  }
+  
+  let user = new User('John');
+  let user2 = new user.constructor('Pete');
+  
+  alert( user2.name ); // Pete 
+

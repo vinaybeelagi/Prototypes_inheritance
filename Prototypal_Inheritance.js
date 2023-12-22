07a9132a26b504d0f81a6dbe12ac6db875cebc12
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-22 15:57:16 +0530

    task-Working with prototype

--- Prototypal_Inheritance.js
+++ Prototypal_Inheritance.js
@@ -40,4 +40,24 @@ let user = {
   console.log(admin.fullName);
   console.log(user.fullName);  
 
+//  task-Working with prototype
+
+let animal = {
+    jumps: null
+  };
+  let rabbit = {
+    __proto__: animal,
+    jumps: true
+  };
+  
+   console.log( rabbit.jumps ); // ? (1)
+  
+  delete rabbit.jumps;
+  
+   console.log( rabbit.jumps ); // ? (2)
+  
+  delete animal.jumps;
+  
+   console.log( rabbit.jumps ); 
+
 
